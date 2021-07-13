ed90ab0618521767a87d762e768687867c9dc008
Maksim Voronovich <maxxam@yandex.ru>
2021-07-13 21:58:09 +0300

    добавил вывод модального сообщение 'Спасибо! Скоро с вами свяжется консультант...'

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-const ConsultationButton = document.querySelector(".header-search-button")
-const PopupBackground = document.getElementById("popup")
-const PopupForm = document.getElementById("form")
+const ConsultationButton = document.querySelector('.header-search-button')
+const SubmitButton = document.querySelector('.input-button')
+const CloseMessageButton = document.querySelector('.message-btn')
+const PopupBackground = document.getElementById('popup')
+const PopupForm = document.getElementById('form')
+const PopupMessage = document.getElementById('message')
 
-ConsultationButton.addEventListener("click", () => {
-	PopupBackground.classList.remove("none")
-	PopupForm.classList.add("form-popup")
+function openModalWindow() {
+	PopupBackground.classList.remove('none')
+  PopupForm.classList.add('form-popup')
+}
+
+function closeModalWindow() {
+	PopupBackground.classList.add('none')
+  PopupForm.classList.remove('form-popup')
+}
+
+function openPopupMessageWindow() {
+	PopupMessage.classList.remove('none')
+}
+
+function closePopupMessageWindow() {
+	PopupMessage.classList.add('none')
+}
+
+ConsultationButton.addEventListener('click', () => {
+  openModalWindow()
 })
 
-PopupBackground.addEventListener("click", () => {
-	PopupBackground.classList.add("none")
-	PopupForm.classList.remove("form-popup")
+PopupBackground.addEventListener('click', () => {
+	closeModalWindow()
 })
 
+SubmitButton.addEventListener('click', () => {
+	closeModalWindow()
+	openPopupMessageWindow()
+})
 
+CloseMessageButton.addEventListener('click', () => {
+	closePopupMessageWindow()
+})
