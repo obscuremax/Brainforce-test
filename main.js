7c1f3fe163e79df99d9b58629533cb740b951fea
Maksim Voronovich <maxxam@yandex.ru>
2021-07-14 07:31:40 +0300

    cделал валидацию формы

--- main.js
+++ main.js
@@ -1,55 +1,68 @@
 const ConsultationButton = document.querySelector('.header-search-button')
-const SubmitButton = document.querySelector('.input-button')
 const CloseMessageButton = document.querySelector('.message-btn')
 const PopupBackground = document.getElementById('popup')
 const PopupForm = document.getElementById('form')
 const PopupMessage = document.getElementById('message')
-const CertificationButton = document.querySelectorAll(".certification-btn")
+const CertificationButton = document.querySelectorAll('.certification-btn')
 
-function openModalWindow() {
-	PopupBackground.classList.remove('none')
-  PopupForm.classList.add('form-popup')
-}
-
-function closeModalWindow() {
-	PopupBackground.classList.add('none')
-  PopupForm.classList.remove('form-popup')
-}
-
-function openPopupMessageWindow() {
-	PopupMessage.classList.remove('none')
-}
-
-function closePopupMessageWindow() {
-	PopupMessage.classList.add('none')
-}
+PopupForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  fetch('/')
+    .then((res) => {
+      if (res.ok) {
+        closeModalWindow()
+      }
+    })
+    .then(openPopupMessageWindow(), cleanInputName(), cleanInputPhone())
+})
 
 ConsultationButton.addEventListener('click', () => {
   openModalWindow()
 })
 
 PopupBackground.addEventListener('click', () => {
-	closeModalWindow()
-})
-
-SubmitButton.addEventListener('click', () => {
-	closeModalWindow()
-	openPopupMessageWindow()
+  closeModalWindow()
 })
 
 CloseMessageButton.addEventListener('click', () => {
-	closePopupMessageWindow()
+  closePopupMessageWindow()
 })
 
-CertificationButton.forEach(button => {
-		button.addEventListener("click", (e) => {
-			e.currentTarget.classList.toggle("certification-btn-rotate")
-			let collections = e.currentTarget.parentNode.children
+CertificationButton.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('certification-btn-rotate')
+    let collections = e.currentTarget.parentNode.children
 
-			for (let el of collections) {
-				if (el.classList.contains("certification-subtitle")) {
-					el.classList.toggle("none")
-				}
-			}
-		})
-	})
+    for (let el of collections) {
+      if (el.classList.contains('certification-subtitle')) {
+        el.classList.toggle('none')
+      }
+    }
+  })
+})
+
+
+function cleanInputName() {
+  document.querySelector('.input-name').value = ''
+}
+function cleanInputPhone() {
+  document.querySelector('.input-phone').value = ''
+}
+
+function openModalWindow() {
+  PopupBackground.classList.remove('none')
+  PopupForm.classList.add('form-popup')
+}
+
+function closeModalWindow() {
+  PopupBackground.classList.add('none')
+  PopupForm.classList.remove('form-popup')
+}
+
+function openPopupMessageWindow() {
+  PopupMessage.classList.remove('none')
+}
+
+function closePopupMessageWindow() {
+  PopupMessage.classList.add('none')
+}
